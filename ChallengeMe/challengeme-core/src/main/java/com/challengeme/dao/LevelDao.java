e039fdf821b25b4891b156b7b335b4659824f78e
Steeve <nkemson@tzi.de>
2014-12-30 16:31:52 +0100

    jai ajoute certains commentaires et change le contenu de certains methodes

--- ChallengeMe/challengeme-core/src/main/java/com/challengeme/dao/LevelDao.java
+++ ChallengeMe/challengeme-core/src/main/java/com/challengeme/dao/LevelDao.java
@@ -1,6 +1,9 @@
 package com.challengeme.dao;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
@@ -8,22 +11,31 @@ import org.hibernate.Transaction;
 
 import com.challengeme.config.HibernateConfigurator;
 import com.challengeme.config.TransaktionContainer;
+import com.challengeme.entities.Answer;
 import com.challengeme.entities.Kategorie;
 import com.challengeme.entities.Level;
+import com.challengeme.entities.MultipleChoice;
 
 public class LevelDao {
-	
-	//private static int count = 0; 
-	
-public Level insert(String levelName, Kategorie... kategories)  {
-		
 
-		//TODO response muss be in answer array
+	/**
+	 * fügt liste von fragen in einem level ein
+	 * 
+	 * @param levelName
+	 * @param multiplechoices
+	 * @return
+	 */
+	public Level insert(String levelName, MultipleChoice... multiplechoices) {
+
 		Transaction tx = TransaktionContainer.getTransaktion();
 		Session session = HibernateConfigurator.getInstance().getSession();
 		try {
 			Level reg = new Level();
 			reg.setLevelName(levelName);
+			for (MultipleChoice m : multiplechoices) {
+				m.setLevel(reg);
+				reg.getMultipleChoices().add(m);
+			}
 			session.save(reg);
 			tx.commit();
 			return reg;
@@ -32,60 +44,70 @@ public Level insert(String levelName, Kategorie... kategories)  {
 				tx.rollback();
 			e.printStackTrace();
 		}
-	   
-	  
-		return null; 
-	
-  }
-	
-	// Method to DELETE a level from the records 
+
+		return null;
+
+	}
+
+	// Method to DELETE a level from the records
 	public void delete(Integer levelID) {
 		Transaction tx = TransaktionContainer.getTransaktion();
 		Session session = HibernateConfigurator.getInstance().getSession();
+
 		try {
-		
 			Level del = (Level) session.get(Level.class, levelID);
-			session.delete(del);
+			if (del.getMultipleChoices() == null) {
+				session.delete(del);
+			} else {
+				for (MultipleChoice c : del.getMultipleChoices()) {
+					c.setLevel(null);
+				}
+				del.setMultipleChoices(null);
+				del.setSpiel(null);
+
+				session.delete(del);
+			}
 			tx.commit();
 		} catch (HibernateException e) {
 			if (tx != null)
 				tx.rollback();
 			e.printStackTrace();
-		} 
+		}
 	}
-	
-	 //Method to update a question from the records 
+
+	/** Method to update a level from the records */
 	public void updateLevel(Integer levelID, String levelName) {
-		
+
 		Transaction tx = TransaktionContainer.getTransaktion();
 		Session session = HibernateConfigurator.getInstance().getSession();
-		
+
 		try {
-			
+
 			Level upd = (Level) session.get(Level.class, levelID);
 			upd.setLevelName(levelName);
 			session.update(upd);
 			tx.commit();
 		} catch (HibernateException e) {
-		
+
 			if (tx != null)
 				tx.rollback();
 			e.printStackTrace();
 
-		} 
+		}
 	}
-	
+    
+	/** method to get all level im spiel */
 	@SuppressWarnings("unchecked")
-	public List<Level> getLevels(){
+	public List<Level> getLevels() {
 		Session session = HibernateConfigurator.getInstance().getSession();
 		try {
-			List<Level> levels = session.createQuery(" select l from Level l ").list();
+			List<Level> levels = session.createQuery(" select l from Level l ")
+					.list();
 			return levels;
 		} catch (HibernateException e) {
 			e.printStackTrace();
-		} 
+		}
 		return null;
 	}
-
 
 }
